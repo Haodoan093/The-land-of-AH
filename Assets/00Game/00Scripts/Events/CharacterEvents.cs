bb190170bd0f2d55a8909c3f45a0f54242d48afd
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2023-09-08 18:01:14 +0700

    aa

---
+++ Assets/00Game/00Scripts/Events/CharacterEvents.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterEvents : MonoBehaviour
+{
+    
+        //character dmg and dmg value
+        public static UnityAction<GameObject, float> characterDamaged;
+
+        //character healed and amount healed
+        public static UnityAction<GameObject, float> characterHealed;
+    
+}
---
+++ Assets/00Game/00Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject damageTextPrefab;
+    public GameObject healthTextPrefab;
+
+    public Canvas gameCanvas;
+
+    private void Awake()
+    {
+        gameCanvas = FindAnyObjectByType<Canvas>();
+
+    }
+    private void OnEnable()
+    {
+        CharacterEvents.characterDamaged += CharacterTookDamage;
+        CharacterEvents.characterHealed += CharacterHealed;
+    }
+    private void OnDisable()
+    {
+        CharacterEvents.characterDamaged -= CharacterTookDamage;
+        CharacterEvents.characterHealed -= CharacterHealed;
+    }
+    public void CharacterTookDamage(GameObject character, float damageReceived)
+    {//mat mau
+        //create text at character hit
+        Vector3 spawnPos = Camera.main.WorldToScreenPoint(character.transform.position);
+
+        TMP_Text tmpText = Instantiate(damageTextPrefab, spawnPos, Quaternion.identity, gameCanvas.transform)
+            .GetComponent<TMP_Text>();
+
+        tmpText.text = damageReceived.ToString();
+    }
+    public void CharacterHealed(GameObject character, float healthRestored)
+    {//hoi mau
+        //create text at character hit
+        Vector3 spawnPos = Camera.main.WorldToScreenPoint(character.transform.position);
+
+        TMP_Text tmpText = Instantiate(healthTextPrefab, spawnPos, Quaternion.identity, gameCanvas.transform)
+            .GetComponent<TMP_Text>();
+
+        tmpText.text = healthRestored.ToString();
+    }
+
+
+}
