f9954a818eff1d2c268b56f25234a8461927422e
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2023-10-01 02:32:07 +0700

    map2

---
+++ Assets/00Game/00Scripts/MovingPlatform.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public bool onGra=false;
+    public float speed;
+    Vector3 targetPos;
+
+    PlayerController playerController;
+    Rigidbody2D rigi;
+    Vector3 moveDirection;
+    Rigidbody2D playerRB;
+
+    public GameObject ways;
+    public Transform[] waypoints;
+    int pointIndex;
+    int pointCount;
+    int direction = 1;
+
+    public float waitDuration;
+
+    private Transform newParent;
+    private Transform oldParent;
+    private void Awake()
+    {
+        playerController = GameManager.Instant.Player.GetComponent<PlayerController>();
+        rigi=GetComponent<Rigidbody2D>();
+        playerRB = GameManager.Instant.Player.GetComponent<Rigidbody2D>();
+        waypoints = new Transform[ways.transform.childCount];
+        for(int i = 0; i < ways.transform.childCount; i++)
+        {
+            waypoints[i]=ways.transform.GetChild(i).gameObject.transform;
+        }
+    }
+    private void Start()
+    {
+        pointIndex = 1;
+        pointCount=waypoints.Length;
+        targetPos = waypoints[1].transform.position;
+      
+        DirectionCalculate();
+    }
+    private void Update()
+    {
+        if(Vector2.Distance(transform.position, targetPos) < 0.05f)
+        {
+            NextPoint();
+        }
+        if (newParent != oldParent)
+        {
+            
+            playerController.transform.parent = newParent;
+            oldParent = newParent;
+        }
+
+    }
+    void NextPoint()
+    {
+        transform.position = targetPos;
+        moveDirection = Vector3.zero;
+        if (pointIndex == pointCount - 1)
+        {
+            direction = -1;
+        }
+        if (pointIndex == 0)
+        {
+            direction = 1;
+        }
+        pointIndex += direction;
+        targetPos = waypoints[pointIndex].transform.position;
+            StartCoroutine(WaitNextPoint());
+    }
+
+    IEnumerator WaitNextPoint()
+    {
+        yield return new WaitForSeconds(waitDuration);
+        DirectionCalculate();
+    }
+    private void FixedUpdate()
+    {
+        rigi.velocity = moveDirection * speed;
+    }
+    void DirectionCalculate()
+    {
+        moveDirection=(targetPos-transform.position).normalized;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerController.isOnPlatform = true;
+            playerController.platformrg = rigi;
+            newParent = transform;
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            newParent = null;
+            playerController.isOnPlatform=false;
+           
+        }
+    }
+}
